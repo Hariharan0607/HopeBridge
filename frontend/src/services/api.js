6ba33fdf32da30aadc82fe7fdacf7cbb18fa0f25
hariharan0607 <hariharanme06@gmail.com>
2026-08-10 19:07:39 +0530

    Fix production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://hopebridge-lwv9.onrender.com/api",
 });
+
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
